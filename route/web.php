8e23b378fd925e636bfd1c62e237ad8730535ada
lhz <l19517863@163.com>
2018-09-27 19:34:38 +0800

    web
    1. 去除 php 结束符 便于 追加写入

--- route/web.php
+++ route/web.php
@@ -2,14 +2,10 @@
 
 use core\Route;
 
+// test
+Route::get('/test/test', 'app/controllers/IndexController@test');
+
 Route::get('/', 'app/controllers/IndexController@index')->name('index');
 Route::get('/index/top', 'app/controllers/IndexController@top')->name('index.top');
 Route::get('/index/menu', 'app/controllers/IndexController@menu')->name('index.menu');
 Route::get('/index/main', 'app/controllers/IndexController@main')->name('index.main');
-
-
-
-
-Route::get('/test/test', 'app/controllers/IndexController@test');
-
-?>
