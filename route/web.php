9297d1ce6e1387ac49129cf2ec0126f4f1cd48ca
lhz <l19517863@163.com>
2018-09-30 20:21:11 +0800

    .
    web User

--- route/web.php
+++ route/web.php
@@ -10,3 +10,12 @@ Route::get('/index/top', 'app/controllers/IndexController@top')->name('index.top
 Route::get('/index/menu', 'app/controllers/IndexController@menu')->name('index.menu');
 Route::get('/index/main', 'app/controllers/IndexController@main')->name('index.main');
         
+// user
+Route::get('/user/index','app/controllers/UserController@index')->name('user.index'); // 显示列表
+Route::get('/user/search','app/controllers/UserController@search')->name('user.search'); // 搜索
+Route::get('/user/add','app/controllers/UserController@add')->name('user.add'); // 显示 添加
+Route::post('/user/insert','app/controllers/UserController@insert')->name('user.insert'); // 添加
+Route::get('/user/del/{id}','app/controllers/UserController@del')->name('user.del'); // 删除
+Route::get('/user/mod/{id}','app/controllers/UserController@mod')->name('user.mod'); // 显示 修改
+Route::post('/user/update/{id}','app/controllers/UserController@update')->name('user.update'); // 修改
+        
