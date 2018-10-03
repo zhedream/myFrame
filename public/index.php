353aeea462327a60e6e649637e50db836acf8414
lhz <l19517863@163.com>
2018-10-03 17:24:36 +0800

    public
    1. 小写

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+// phpinfo();die;
+// ini_set('display_errors', 'On'); // Off
 ini_set('date.timezone', 'PRC');
 ini_set('session.gc_maxlifetime', "1800"); // 秒
 // ini_set("session.cookie_lifetime","3600"); // 秒
