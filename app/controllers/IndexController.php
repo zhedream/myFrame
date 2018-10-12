2ec28851f71747e9eb72860a840dc0a1b67e3665
lhz <l19517863@163.com>
2018-10-12 09:02:12 +0800

    test 初始化

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -3,108 +3,67 @@
 namespace app\controllers;
 
 use core\Request;
-use app\models\Index;
-use app\models\User;
-use app\models\GoodsAttribute;
+use \Index;
 
 class IndexController extends Controller {
 
     // 显示列表
     function index() {
-        // echo 'iframe 不能有其他输出';
         $index = new Index;
-        $info = $index->welcome();
-        view('index.index');
-    }
-
-    function top () {
-
-        view('index.top');
-    }
-
-    function test() {
-
-        $user = new User;
-        
-        $user->fill(['email'=>'123','name'=>'河是德国','money'=>0,'avatar'=>'asdf','password'=>md5('123123')]);
-
-        $user->dd = 1;
-        // dd($user->dd);
-        // var_dump($user);die;
-        // $data = $user->whereIn('id',[1])
-            // ->toSql();
-            // ->get();
-        // dd($data);die;
-        $data = [
-            'asd'=>123,
-        ];
-        // $data =  $user->findOne('select * from users where id=?',[1]);
-        // dd($data);
-        // $user->otest(['id','<=',2]);
-        // dd(1,false);
-        $data = $user
-                // ->where(['id'=>1])
-                // ->where('money','>=',3)
-                ->Where('name','LIKE',"%liu%")
-                // ->where('display3','LIKE',"'%title%'")
-                // ->orWhere(['a'=>4])
-                // ->orWhere('b','NOT LIKE',"'%asd%'")
-                // ->orWhere('c',6)
-                // ->whereIn('d',[6,99],'not')
-                // ->whereIn('id',function($s){
-                //     // 'select `user_id` from users'
-                //     return $s->where('display','>=',2)->orWhereIn('id',[123,2],true);
-                // })
-                // ->whereIn('d',function(){})
-                // ->toSql();
-                ->get();
-        dd($data);
-    }
-
-    function menu () {
-
-        view('index.menu');
-    }
-
-    function main () {
-
-        view('index.main');
+        $data = $index->get();
+        view('index.index',['data'=>$data]);
     }
 
     // 显示 添加页
     function add(){
-
+        
         view('index.create');
     }
 
     // 添加
     function insert(Request $req,$id) {
-        
-        
+
+        $data = $req->all();
+        $index = new Index;
+        $index->fill($data);
+        $index->exec_insert($index->getFillData());
+        message('数据添加成功',1,Route('index.index'),3);
+        // redirect(Route('index.index'));
     }
 
     // 删除
     function del(Request $req,$id){
-        
 
+        // $data = $req->all();
+        $index = new Index;
+        $index->where($id)
+            ->delete();
+        message('数据删除成功',1,Route('index.index'),3);
     }
     
     // 显示 修改页
-    function mod(){
-        
-        view('index.edit');
+    function mod(Request $req,$id){
+
+        $data = $req->all();
+        $index = new Index;
+        $data = $index->where($id)->get()[0];
+        view('index.edit',['data'=>$data]);
     }
 
     // 修改
     function update(Request $req,$id) {
-
-
+        
+        $data = $req->all();
+        $index = new Index;
+        $index->where($id)
+            ->fill($data)
+            ->update();
+        message('数据更改成功',1,Route('index.index'),3);
+        
     }
 
     // 搜索
     function search(Request $req,$id){
-
-
     }
 
 }
--- route/web.php
+++ route/web.php
@@ -20,45 +20,9 @@ Route::get('/index/main', 'app/controllers/IndexController@main')->name('index.m
 Route::middleware(['CheckLogin'],function(){
     
 
-
 // 加锁 开启权限验证
 Route::lock(function(){
 
-// user
-Route::get('/user/index','app/controllers/UserController@index')->name('user.index'); // 显示列表
-Route::get('/user/search','app/controllers/UserController@search')->name('user.search'); // 搜索
-Route::get('/user/add','app/controllers/UserController@add')->name('user.add'); // 显示 添加
-Route::post('/user/insert','app/controllers/UserController@insert')->name('user.insert'); // 添加
-Route::get('/user/del/{id}','app/controllers/UserController@del')->name('user.del'); // 删除
-Route::get('/user/mod/{id}','app/controllers/UserController@mod')->name('user.mod'); // 显示 修改
-Route::post('/user/update/{id}','app/controllers/UserController@update')->name('user.update'); // 修改
-
-// category
-Route::get('/category/index','app/controllers/CategoryController@index')->name('category.index'); // 显示列表
-Route::get('/category/search','app/controllers/CategoryController@search')->name('category.search'); // 搜索
-Route::get('/category/add','app/controllers/CategoryController@add')->name('category.add'); // 显示 添加
-Route::post('/category/insert','app/controllers/CategoryController@insert')->name('category.insert'); // 添加
-Route::get('/category/del/{id}','app/controllers/CategoryController@del')->name('category.del'); // 删除
-Route::get('/category/mod/{id}','app/controllers/CategoryController@mod')->name('category.mod'); // 显示 修改
-Route::post('/category/update/{id}','app/controllers/CategoryController@update')->name('category.update'); // 修改
-
-// brand
-Route::get('/brand/index','app/controllers/BrandController@index')->name('brand.index'); // 显示列表
-Route::get('/brand/search','app/controllers/BrandController@search')->name('brand.search'); // 搜索
-Route::get('/brand/add','app/controllers/BrandController@add')->name('brand.add'); // 显示 添加
-Route::post('/brand/insert','app/controllers/BrandController@insert')->name('brand.insert'); // 添加
-Route::get('/brand/del/{id}','app/controllers/BrandController@del')->name('brand.del'); // 删除 post
-Route::get('/brand/mod/{id}','app/controllers/BrandController@mod')->name('brand.mod'); // 显示 修改
-Route::post('/brand/update/{id}','app/controllers/BrandController@update')->name('brand.update'); // 修改
-
-// goods
-Route::get('/goods/index','app/controllers/GoodsController@index')->name('goods.index'); // 显示列表
-Route::get('/goods/search','app/controllers/GoodsController@search')->name('goods.search'); // 搜索
-Route::get('/goods/add','app/controllers/GoodsController@add')->name('goods.add'); // 显示 添加
-Route::post('/goods/insert','app/controllers/GoodsController@insert')->name('goods.insert'); // 添加
-Route::get('/goods/del/{id}','app/controllers/GoodsController@del')->name('goods.del'); // 删除 post
-Route::get('/goods/mod/{id}','app/controllers/GoodsController@mod')->name('goods.mod'); // 显示 修改
-Route::post('/goods/update/{id}','app/controllers/GoodsController@update')->name('goods.update'); // 修改
 
 });
 
@@ -66,55 +30,13 @@ Route::post('/goods/update/{id}','app/controllers/GoodsController@update')->name
 
 // var_dump(Route::$gets,Route::$posts);die;
 
-
-// privilege
-Route::get('/privilege/base','app/controllers/PrivilegeController@base')->name('privilege.base'); // 基本权限
-Route::get('/privilege/index','app/controllers/PrivilegeController@index')->name('privilege.index'); // 显示列表
-Route::get('/privilege/search','app/controllers/PrivilegeController@search')->name('privilege.search'); // 搜索
-Route::get('/privilege/add','app/controllers/PrivilegeController@add')->name('privilege.add'); // 显示 添加
-Route::post('/privilege/insert','app/controllers/PrivilegeController@insert')->name('privilege.insert'); // 添加
-Route::get('/privilege/del/{id}','app/controllers/PrivilegeController@del')->name('privilege.del'); // 删除 post
-Route::get('/privilege/mod/{id}','app/controllers/PrivilegeController@mod')->name('privilege.mod'); // 显示 修改
-Route::post('/privilege/update/{id}','app/controllers/PrivilegeController@update')->name('privilege.update'); // 修改
-
-// role
-Route::get('/role/index','app/controllers/RoleController@index')->name('role.index'); // 显示列表
-Route::get('/role/search','app/controllers/RoleController@search')->name('role.search'); // 搜索
-Route::get('/role/add','app/controllers/RoleController@add')->name('role.add'); // 显示 添加
-Route::post('/role/insert','app/controllers/RoleController@insert')->name('role.insert'); // 添加
-Route::get('/role/del/{id}','app/controllers/RoleController@del')->name('role.del'); // 删除 post
-Route::get('/role/mod/{id}','app/controllers/RoleController@mod')->name('role.mod'); // 显示 修改
-Route::post('/role/update/{id}','app/controllers/RoleController@update')->name('role.update'); // 修改
         
-// admin
-Route::get('/admin/index','app/controllers/AdminController@index')->name('admin.index'); // 显示列表
-Route::get('/admin/search','app/controllers/AdminController@search')->name('admin.search'); // 搜索
-Route::get('/admin/add','app/controllers/AdminController@add')->name('admin.add'); // 显示 添加
-Route::post('/admin/insert','app/controllers/AdminController@insert')->name('admin.insert'); // 添加
-Route::get('/admin/del/{id}','app/controllers/AdminController@del')->name('admin.del'); // 删除 post
-Route::get('/admin/mod/{id}','app/controllers/AdminController@mod')->name('admin.mod'); // 显示 修改
-Route::post('/admin/update/{id}','app/controllers/AdminController@update')->name('admin.update'); // 修改
-        
-// login
-Route::get('/login/index','app/controllers/LoginController@index')->name('login.index'); // 显示登陆页
-Route::get('/login/logout','app/controllers/LoginController@logout')->name('login.logout'); // 退出
-Route::post('/login/login','app/controllers/LoginController@login')->name('login.login'); // 登陆验证
-        
-// article
-Route::get('/article/index','app/controllers/ArticleController@index')->name('article.index'); // 显示列表
-Route::get('/article/search','app/controllers/ArticleController@search')->name('article.search'); // 搜索
-Route::get('/article/add','app/controllers/ArticleController@add')->name('article.add'); // 显示 添加
-Route::post('/article/insert','app/controllers/ArticleController@insert')->name('article.insert'); // 添加
-Route::get('/article/del/{id}','app/controllers/ArticleController@del')->name('article.del'); // 删除 post
-Route::get('/article/mod/{id}','app/controllers/ArticleController@mod')->name('article.mod'); // 显示 修改
-Route::post('/article/update/{id}','app/controllers/ArticleController@update')->name('article.update'); // 修改
-        
-// articlecategory
-Route::get('/articlecategory/index','app/controllers/ArticleCategoryController@index')->name('articlecategory.index'); // 显示列表
-Route::get('/articlecategory/search','app/controllers/ArticleCategoryController@search')->name('articlecategory.search'); // 搜索
-Route::get('/articlecategory/add','app/controllers/ArticleCategoryController@add')->name('articlecategory.add'); // 显示 添加
-Route::post('/articlecategory/insert','app/controllers/ArticleCategoryController@insert')->name('articlecategory.insert'); // 添加
-Route::get('/articlecategory/del/{id}','app/controllers/ArticleCategoryController@del')->name('articlecategory.del'); // 删除 post
-Route::get('/articlecategory/mod/{id}','app/controllers/ArticleCategoryController@mod')->name('articlecategory.mod'); // 显示 修改
-Route::post('/articlecategory/update/{id}','app/controllers/ArticleCategoryController@update')->name('articlecategory.update'); // 修改
+// index
+Route::get('/index/index','app/controllers/IndexController@index')->name('index.index'); // 显示列表
+Route::get('/index/search','app/controllers/IndexController@search')->name('index.search'); // 搜索
+Route::get('/index/add','app/controllers/IndexController@add')->name('index.add'); // 显示 添加
+Route::post('/index/insert','app/controllers/IndexController@insert')->name('index.insert'); // 添加
+Route::get('/index/del/{id}','app/controllers/IndexController@del')->name('index.del'); // 删除 post
+Route::get('/index/mod/{id}','app/controllers/IndexController@mod')->name('index.mod'); // 显示 修改
+Route::post('/index/update/{id}','app/controllers/IndexController@update')->name('index.update'); // 修改
         
