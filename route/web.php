4b78e57b7f59e64478f2f2f413e37efacff4bfda
lhz <l19517863@163.com>
2018-10-12 16:56:17 +0800

    添加字体

--- route/web.php
+++ route/web.php
@@ -12,14 +12,21 @@ Route::get('/', 'app/controllers/IndexController@index')->name('index');
 Route::middleware(['CheckLogin'],function(){
     Route::get('/index/index','app/controllers/IndexController@index')->name('index.index'); // 显示首页
 
+// article
+Route::get('/article/index','app/controllers/ArticleController@index')->name('article.index'); // 显示列表
+Route::get('/article/search','app/controllers/ArticleController@search')->name('article.search'); // 搜索
+Route::get('/article/add','app/controllers/ArticleController@add')->name('article.add'); // 显示 添加
+Route::post('/article/insert','app/controllers/ArticleController@insert')->name('article.insert'); // 添加
+Route::get('/article/del/{id}','app/controllers/ArticleController@del')->name('article.del'); // 删除 post
+Route::get('/article/mod/{id}','app/controllers/ArticleController@mod')->name('article.mod'); // 显示 修改
+Route::post('/article/update/{id}','app/controllers/ArticleController@update')->name('article.update'); // 修改
+
 });
 
 Route::get('/user/login','app/controllers/UserController@login')->name('user.login'); // 显示 登陆
 
-Route::middleware(['PassErrTime'],function(){
-    Route::post('/user/login','app/controllers/UserController@dologin')->name('user.dologin'); // 登陆
-});
 
+Route::post('/user/login','app/controllers/UserController@dologin')->middleware('PassErrTime')->name('user.dologin'); // 登陆
 
 Route::get('/user/reg','app/controllers/UserController@reg')->name('user.reg'); // 邮箱注册
 Route::get('/user/reg2','app/controllers/UserController@reg2')->name('user.reg2'); // 手机注册
@@ -30,11 +37,4 @@ Route::post('/user/reg2','app/controllers/UserController@doreg2')->name('user.do
 // code
 Route::get('/code/Captcha','app/controllers/CodeController@getCaptcha')->name('code.Captcha'); // 验证码
     
-// article
-Route::get('/article/index','app/controllers/ArticleController@index')->name('article.index'); // 显示列表
-Route::get('/article/search','app/controllers/ArticleController@search')->name('article.search'); // 搜索
-Route::get('/article/add','app/controllers/ArticleController@add')->name('article.add'); // 显示 添加
-Route::post('/article/insert','app/controllers/ArticleController@insert')->name('article.insert'); // 添加
-Route::get('/article/del/{id}','app/controllers/ArticleController@del')->name('article.del'); // 删除 post
-Route::get('/article/mod/{id}','app/controllers/ArticleController@mod')->name('article.mod'); // 显示 修改
-Route::post('/article/update/{id}','app/controllers/ArticleController@update')->name('article.update'); // 修改
+
