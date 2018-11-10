24cd0c376ae7b0058a8863714e956958c9b342ce
lhz <l19517863@163.com>
2018-11-10 17:32:10 +0800

    v 2.0

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,12 +4,20 @@ namespace app\controllers;
 
 use core\Request;
 
+use app\models\User;
 class IndexController extends Controller {
 
     // 显示列表
     function index() {
-
+        
         view('index',['info'=>'welcome to myFrame']);
+    }
+
+    function user(){
+        
+        $u = new User;
+        $data = $u->get();
+        dd($data);
     }
 
 }
--- route/web.php
+++ route/web.php
@@ -3,4 +3,5 @@
 use core\Route;
 
 Route::get('/','app/controllers/IndexController@index')->name('index.index');
+Route::get('/user','app/controllers/IndexController@user')->name('index.user');
         
