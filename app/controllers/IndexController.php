61151302ed6ee504165358498d9f151e1b185c9e
lhz <l19517863@163.com>
2018-11-10 16:10:48 +0800

    v 2.0 clean

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,60 +8,8 @@ class IndexController extends Controller {
 
     // 显示列表
     function index() {
-        
-        view('index.index');
-    }
 
-    // 显示 添加页
-    function add(){
-        
-        view('index.create');
-    }
-
-    // 添加
-    function insert(Request $req,$id) {
-
-        $data = $req->all();
-        $index = new Index;
-        $index->fill($data);
-        $index->exec_insert($index->getFillData());
-        message('数据添加成功',1,Route('index.index'),3);
-        // redirect(Route('index.index'));
-    }
-
-    // 删除
-    function del(Request $req,$id){
-
-        // $data = $req->all();
-        $index = new Index;
-        $index->where($id)
-            ->delete();
-        message('数据删除成功',1,Route('index.index'),3);
-    }
-    
-    // 显示 修改页
-    function mod(Request $req,$id){
-
-        $data = $req->all();
-        $index = new Index;
-        $data = $index->where($id)->get()[0];
-        view('index.edit',['data'=>$data]);
-    }
-
-    // 修改
-    function update(Request $req,$id) {
-        
-        $data = $req->all();
-        $index = new Index;
-        $index->where($id)
-            ->fill($data)
-            ->update();
-        message('数据更改成功',1,Route('index.index'),3);
-        
-    }
-
-    // 搜索
-    function search(Request $req,$id){
+        view('index',['info'=>'welcome to myFrame']);
     }
 
 }
